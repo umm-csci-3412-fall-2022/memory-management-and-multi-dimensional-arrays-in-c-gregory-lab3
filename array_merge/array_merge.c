e905eff316ba953802db25228b02585f8fe78c87
Gregory Peterson <pet03356@morris.umn.edu>
2022-12-02 17:51:27 -0600

    Finished

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,47 +1,67 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
 #include <cstdlib>
+#include <stdio.h>
 
 int totalNumElements(int* sizes, int num_arrays){
-	int count = 0;
-	for(int i=0; i<num_arrays; ++i){
-		count = (count + sizes[i]);
-		}
-}
-
-bool notDuplicate(int num, int* resultArray, currentResultIndex){
-	bool boole = true
-	for(int i=0; i<=currentResultIndex; ++i){
-		if(num == mergingarray[i]){
-		boole = false
-		}
-	}
-	return boole;
+        int count = 0;
+        for(int i=0; i<num_arrays; ++i){
+                count = (count + sizes[i]);
+                }
+        return count;
 }
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  	int resultSize = totalNumElements(sizes, num_arrays);
+        int size = totalNumElements(sizes, num_arrays);
 
-	int* result = (int*) calloc(resultSize, sizeof(int));
+        if(num_arrays == 0){
+                int* emptyArray = (int*) calloc(1, sizeof(int));
+                emptyArray[0] = 0;
+                return emptyArray;
+                }
 
-	int currentArraySize = 0;
-	int currentResultIndex = 0;
+        int currentArraySize = 0;
+        int currentIndex = 0;
+        int* temp =(int*)calloc(size, sizeof(int));
 
-	for (int i=0; i<num_arrays; ++i){
-		currentArraySize = sizes[i];
-		for (int j=0; j<currentArraySize; ++j){
-			if(notDuplicate(values[i][j], result, currentResultIndex)){
-				result[currentResultIndex]= values[i][j];
-				currentResultIndex++
-				}
-		}
-	}
-	// Resize according to how many nonduplicates were added +1 slot for the num of non duplicate elements.
-	int* finalResult = (int*) calloc(currentResultIndex+1, sizeof(int));
-	finalResult[0] = currentResultIndex+1;
 
-	for(int i=0, i<=currentResultIndex; ++i){
-		finalResult[i+1] = result[i];
-	}
-	return finalResult;
+        for (int i=0; i<num_arrays; ++i){
+                currentArraySize = sizes[i];
+                for (int j=0; j<currentArraySize; ++j){
+                        temp[currentIndex]= values[i][j];
+                        ++currentIndex;
+                }
+        }
+        // Mergesort it, count the number of nonduplicates to know the size of the final array, and then 
+        // only add nonduplicates to final array.
+        mergesort(currentIndex, temp);
+
+        int nonDuplicates = 1;
+        int currentNonDuplicate = temp[0];
+        for(int f=0; f<size; ++f){
+                if(currentNonDuplicate != temp[f]){
+                        currentNonDuplicate = temp[f];
+                        nonDuplicates++;
+                }
+        }
+
+
+        // To create a slot for k, the total number of elements.
+
+        int* dArray = (int*) calloc(nonDuplicates+1, sizeof(int));
+        // Start by adding the number of non duplicates to the array and the first non duplicate element.
+        dArray[0] = nonDuplicates;
+        dArray[1] = temp[0];
+        currentNonDuplicate = temp[0];
+        int index = 2;
+
+        for(int f=1; f<size; ++f){
+                if(currentNonDuplicate != temp[f]){
+                        currentNonDuplicate = temp[f];
+			dArray[index] = temp[f];
+                        index++;
+                }
+        }
+        free(temp);
+        return dArray;
 }
